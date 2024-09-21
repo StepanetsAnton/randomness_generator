09e45a05f5e644f4683ce6653174ebe1e1b36156
anton_stepanets <anton.stepanets15@gmail.com>
2024-09-21 15:20:21 +0200

    Initial commit

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -1,6 +1,5 @@
 user_data = ""
 temp = ""
-
 while len(user_data)<100:
     prompt = str(input("Print a random string containing 0 or 1:"))
 
@@ -10,7 +9,6 @@ while len(user_data)<100:
 
 
     print("Current data length is ", len(user_data),",",(100-len(user_data)),"symbols left")
-
 print()
 print("Final data string:")
 print(str(user_data))
