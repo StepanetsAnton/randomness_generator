888b8e842b91e71e79a3bd188fc32331c68007ff
anton_stepanets <anton.stepanets15@gmail.com>
2024-09-23 10:39:51 +0200

    2nd task with splitting user input and calculating probabilities of symbol occurrence

--- Generating Randomness/task/predictor/predictor.py
+++ Generating Randomness/task/predictor/predictor.py
@@ -12,3 +12,30 @@ while len(user_data)<100:
 print()
 print("Final data string:")
 print(str(user_data))
+
+triads = ["000", "001", "010", "011", "100", "101", "110", "111"]
+
+sliced = [user_data[symbol:symbol+4] for symbol in range(0, len(user_data))]
+
+
+triads_followed_by_0 = {"000":0}
+for triad in triads:
+    triads_followed_by_0.update({triad:0})
+triads_followed_by_1 = {"000":0}
+for triad in triads:
+    triads_followed_by_1.update({triad:0})
+
+for element in sliced:
+    if len(element)==4:
+        for triad in triads:
+            if element.startswith(triad):
+                if element[3] == "0":
+                    temp = triads_followed_by_0[triad]
+                    triads_followed_by_0.update({triad:(temp+1)})
+                else:
+                    temp=triads_followed_by_1[triad]
+                    triads_followed_by_1.update({triad:(temp+1)})
+
+print()
+for triad in triads:
+    print(triad+": "+str(triads_followed_by_0[triad])+","+str(triads_followed_by_1[triad]))
